25d64a7dca22f1b2fa91c4e72682996f1364865d
Awanprasetya <kurniawan.saepudin21@gmail.com>
2025-09-10 11:48:36 +0700

    perubahan tampilan index dan view disesuaikan dengan tampil po detail

--- application/views/materials/view.php
+++ application/views/materials/view.php
@@ -1,73 +1,75 @@
+
+
+
 <?= card_open('<i class="icon cil-spreadsheet"></i> Detail Materials') ?>
-    <table class="table table-bordered">
-        
-    <tr>
-        <th>id</th>
-        <td><?= $row->id ?></td>
-    </tr>
-    <tr>
-        <th>kode</th>
-        <td><?= $row->kode ?></td>
-    </tr>
-    <tr>
-        <th>nama</th>
-        <td><?= $row->nama ?></td>
-    </tr>
-    <tr>
-        <th>qty</th>
-        <td><?= $row->qty ?></td>
-    </tr>
-    <tr>
-        <th>jenis</th>
-        <td><?= $row->jenis ?></td>
-    </tr>
-    <tr>
-        <th>lokasi_1</th>
-        <td><?= $row->lokasi_1 ?></td>
-    </tr>
-    <tr>
-        <th>lokasi_2</th>
-        <td><?= $row->lokasi_2 ?></td>
-    </tr>
-    <tr>
-        <th>keterangan</th>
-        <td><?= $row->keterangan ?></td>
-    </tr>
-    <tr>
-        <th>is_deleted</th>
-        <td><?= $row->is_deleted ?></td>
-    </tr>
-    <tr>
-        <th>created_by</th>
-        <td><?= $row->created_by ?></td>
-    </tr>
-    <tr>
-        <th>updated_by</th>
-        <td><?= $row->updated_by ?></td>
-    </tr>
-    <tr>
-        <th>deleted_by</th>
-        <td><?= $row->deleted_by ?></td>
-    </tr>
-    <tr>
-        <th>created_at</th>
-        <td><?= $row->created_at ?></td>
-    </tr>
-    <tr>
-        <th>updated_at</th>
-        <td><?= $row->updated_at ?></td>
-    </tr>
-    <tr>
-        <th>deleted_at</th>
-        <td><?= $row->deleted_at ?></td>
-    </tr>
-    </table>
-    <div class="mt-3">
-        <div class="btn-group" role="group" aria-label="FormCreateUpdate">
+    
+    <div class="row">
+        <div class="col-8">
+    </div>
+    <div class="col-4 text-end">
+    <div class="mb-3">
+             <div class="btn-group" role="group" aria-label="FormCreateUpdate">
             <a href="<?= site_url('materials/edit/'.$row->id) ?>" class="btn btn-warning" data-coreui-toggle="tooltip" data-coreui-placement"top" title="Edit Data Ini"><i class="icon cil-pencil"></i> Edit</a>
             <a href="<?= site_url('materials/delete/'.$row->id) ?>" class="btn btn-danger" onclick="return confirm('Hapus data ini?')" data-coreui-toggle="tooltip" data-coreui-placement="top" title="Hapus Data Ini"><i class="icon cil-trash"></i> Delete</a>
             <a href="<?= site_url('materials') ?>" class="btn btn-secondary" data-coreui-toggle="tooltip" data-coreui-placement="top" title="< Kembali ke List Data"><i class="icon cil-reload"></i> Kembali</a>
             <a href="<?= site_url('/') ?>" class="btn btn-outline-dark" data-coreui-toggle="tooltip" data-coreui-placement="top" title="< Kembali ke Halaman Utama"><i class="icon cil-home"></i> </a>
         </div>
+        </div>
+    </div>
+    
+</div>
+    <table class="table table-bordered">
+        
+    <tr>
+        <th>ID</th>
+        <td><?= $row->id ?></td>
+    </tr>
+    <tr>
+        <th>KODE</th>
+        <td><?= $row->kode ?></td>
+    </tr>
+    <tr>
+        <th>NAMA</th>
+        <td><?= $row->nama ?></td>
+    </tr>
+    <tr>
+        <th>QTY</th>
+        <td><?= $row->qty ?></td>
+    </tr>
+    <tr>
+        <th>JENIS</th>
+        <td><?= $row->jenis ?></td>
+    </tr>
+    <tr>
+        <th>LOKASI 1</th>
+        <td><?= $row->lokasi_1 ?></td>
+    </tr>
+    <tr>
+        <th>LOKASI 2</th>
+        <td><?= $row->lokasi_2 ?></td>
+    </tr>
+    <tr>
+        <th>KET</th>
+        <td><?= $row->keterangan ?></td>
+    </tr>
+    
+    </table>
+   <?php
+
+
+
+// Optional: table attributes for styling (e.g., using Bootstrap)
+$table_attributes = array(
+    'class' => 'table table-bordered table-hover border'
+);
+
+?>
+    <?= card_close() ?>
+    
+
+    <div class="row mt-3">
+        <div class="col-12">Informasi Data</div>
+        <div class="col-4"><?=generate_card_info(indo_date($row->created_at, true).' > Created By', $row->created_by, 'primary');?></div>
+        <div class="col-4"><?=generate_card_info(indo_date($row->updated_at, true).' > Updated By', $row->updated_by);?></div>   
     </div>
 <?= card_close() ?>
